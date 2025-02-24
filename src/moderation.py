d2756068457963a6d4b1cf86a54f41f00e34a092
a17sol <mishasolovyev@yandex.ru>
2025-02-24 13:35:17 +0200

    improve blocked-by-user safety

--- src/moderation.py
+++ src/moderation.py
@@ -16,20 +16,20 @@ async def check_inactivity(context):
 
 		if (game := chat_data.get('game')) and game.move_start_time + 900 < current_time:
 			user = chat_data['game'].current_user()
+			chat_context = CallbackContext(context.application, chat_id=chat_id)
+			await kick(user, chat_context)
 			await context.bot.send_message(
 				chat_id=chat_id, 
 				text=user.mention_html() + " кикнут(а), так как не закончил(а) ход за 15 минут.", 
 				parse_mode='html'
 			)
-			chat_context = CallbackContext(context.application, chat_id=chat_id)
-			await kick(user, chat_context)
 
 		elif (inv := chat_data.get('invite')) and inv.creation_time + 900 < current_time:
+			del chat_data['invite']
 			await context.bot.send_message(
 				chat_id=chat_id, 
 				text="Приглашение удалено, так как игра не началась за 15 минут.", 
 				parse_mode='html'
 			)
-			del chat_data['invite']
 			await safe_delete(chat_data['board'])
 
